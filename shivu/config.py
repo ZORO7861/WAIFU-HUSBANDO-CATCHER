f0bb8fb32779ed31294027452850f41d062da83d
ZORO7861 <prootaku786@gmail.com>
2025-05-16 05:15:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = ["7795212861", "5758240622", "7361967332", "8019277081"]  # ✅ list of integers
     GROUP_ID = -1002680733244
 
-    TOKEN = "7525650740:AAHujfrXw8A4F3YthCemnWm5re28AbPcYBY"
+    TOKEN = "7007221986:AAHKBWKG13doUv9VrlsIGOe0r_ZOf2hbGdg"
     mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
     PARTNER = "7361967332", "5758240622", "7795212861"
     PHOTO_URL = ["https://files.catbox.moe/h0qot3.jpg", "https://files.catbox.moe/tx0o73.jpg", "https://files.catbox.moe/rqwt42.jpg"]
