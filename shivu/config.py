09a64e241ca0584c8806ca9560edf6e2cd4da972
ZORO7861 <prootaku786@gmail.com>
2025-05-16 05:28:20 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002680733244
 
     TOKEN = "7007221986:AAHKBWKG13doUv9VrlsIGOe0r_ZOf2hbGdg"
-    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
+    mongo_url = "mongodb+srv://akshitmusic59:deathnote0p@cluster0.n1mam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/"
     PARTNER = "7361967332", "5758240622", "7795212861"
     PHOTO_URL = ["https://files.catbox.moe/h0qot3.jpg", "https://files.catbox.moe/tx0o73.jpg", "https://files.catbox.moe/rqwt42.jpg"]
     SUPPORT_CHAT = "hwkwjieie"
